04cc75fa314c2053d798820e2bb70b10336487ef
HirunaHHM <hirunamalavipathirana.333@gmail.com>
2026-05-05 23:14:32 +0530

    fix : removed the exisiting frontend

--- frontend/mock/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './bins';
-export * from './zones';
-export * from './analytics';
-export * from './alerts';
-export * from './route';
-export * from './vehicles';
-export * from './jobs';
